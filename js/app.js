72ecb04589a284e66d088202f68b45b60f46caf2
Genievot <40867747+genievot@users.noreply.github.com>
2019-09-24 22:31:55 +0530

    Added js
    Added js file cuurently can do:
    - Value formatting

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ let dataFromApi = fetch('https://api.celsius.network/api/v3/web/supported_curren
   if (data && data.length > 0) {
     data.forEach(crypto => {
       listOfValuesOne.push({name: crypto.name,usd: crypto.usd, interest: crypto.interestRate})
+      console.log(crypto)
     })
   } else {
     console.log('No data available!')
@@ -38,8 +39,24 @@ var collateralAmmount = document.getElementById('collateral-amount')
 
 // define events
 loanAmmount.addEventListener('keyup', calculate)
-collateralAmmount.addEventListener('keyup', calculateCollateralAmmount)
+// collateralAmmount.addEventListener('keyup', calculateCollateralAmmount)
+
+// Formatting Input Values
+function formatNumber(n) {
+  // format number 1000000 to 1,234,567
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+function format(value) {
+  if (value >= 1000000000) return (value / 1000000000).toFixed(2) + 'B';
+  else if (value >= 1000000) return (value / 1000000).toFixed(2) + 'M';
+}
+
 
 function calculate () {
-
+  let us_value = formatNumber(loanAmmount.value) // "$1,000.00"
+  loanAmmount.value = '$' + us_value
+  let value_raw = loanAmmount.value.match(/\d+/g).join("")
+  console.log(value_raw)
 }
+
+
