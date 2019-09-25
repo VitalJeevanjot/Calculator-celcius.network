97c20f554b4664da5a31fd72811ac7a85b73ede8
Genievot <40867747+genievot@users.noreply.github.com>
2019-09-25 23:45:56 +0530

    JS Updated
    Collateral and Loan Amount Calculating Itself
    Behaviour mode added for ease

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let listOfValuesOne = [];
+let changeBasedOnLA = true;
 
 function latestData() {
   let dataFromApi = fetch('https://api.celsius.network/api/v3/web/supported_currencies')
@@ -9,7 +10,7 @@ function latestData() {
       if (data && data.length > 0) {
         data.forEach(crypto => {
           listOfValuesOne.push({ name: crypto.name, usd: crypto.usd, interest: crypto.interestRate })
-          calculate()
+          calculate_ca()
           console.log(crypto)
         })
       } else {
@@ -23,7 +24,7 @@ latestData();
 
 // select elements
 var loanAmount = document.getElementById('loan-amount')
-var collateralAmmount = document.getElementById('collateral-amount')
+var collateralAmount = document.getElementById('collateral-amount')
 // results cel
 
 // results fiat
@@ -36,18 +37,26 @@ var collateralAmmount = document.getElementById('collateral-amount')
 
 // main logic calculator
 
-// define events
-loanAmount.addEventListener('keyup', calculate)
-collateralAmmount.addEventListener('keyup', calculate)
+// define lazy load events (on blur)
+loanAmount.addEventListener('blur', calculate_ca)
+collateralAmount.addEventListener('blur', calculate_la)
 //Jquery handlers
 $(".currency-btns label").click(function() {
   $(this).addClass('active').siblings().removeClass('active');
-  calculate()
+  if (changeBasedOnLA) {
+    calculate_ca()
+  } else {
+    calculate_la()
+  }
 });
 
 $(".collateral-ltv-btns label").click(function() {
   $(this).addClass('active').siblings().removeClass('active');
-  calculate()
+  if (changeBasedOnLA) {
+    calculate_ca()
+  } else {
+    calculate_la()
+  }
 });
 
 
@@ -62,12 +71,10 @@ let formatter = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 0
 });
 
-function calculate() {
+function calculate_ca() {
   let value_raw_la = loanAmount.value.match(/\d+/g)
-  let value_raw_ca = collateralAmmount.value.match(/\d+/g)
-  
   let joined_value
-  if (!value_raw_la || !value_raw_ca) {
+  if (!value_raw_la) {
     loanAmount.value = '$1,500'
     setColleteralValue(1500)
   }
@@ -75,6 +82,7 @@ function calculate() {
     joined_value = value_raw_la.join("")
     // console.log(joined_value)
     loanAmount.value = formatter.format(joined_value)
+    changeBasedOnLA = true
     setColleteralValue(joined_value)
   }
   if (joined_value < 1500) {
@@ -88,8 +96,25 @@ function calculate() {
     // Get all coin rates at start (Show loading and toasts if not loaded)
     // If user enter colleteral value and do blur on inp field then get latest coin value (To show fast output to user for next time with latest price)
   }
-
 }
+
+function calculate_la() {
+  let value_raw_ca = collateralAmount.value.match(/(\d+(\.\d+)?)/g)
+  
+  let joined_value
+  if (!value_raw_ca) {
+    loanAmount.value = '$1,500'
+    calculate_ca()
+  }
+  else {
+    joined_value = value_raw_ca.join("")
+    // console.log(joined_value)
+    collateralAmount.value = parseInt(joined_value, 10)
+    changeBasedOnLA = false
+    setLoanValue(joined_value)
+  }
+}
+
 function setColleteralValue(loan_amount) {
   let init_currency = $('.currency-btns > .btn.active').text().trim()
   let init_ltv = $('.collateral-ltv-btns > .btn.active').text().trim()
@@ -99,7 +124,22 @@ function setColleteralValue(loan_amount) {
   for (let index = 0; index < elements.length; index++) {
     if (elements[index].name === init_currency) {
       console.log(elements[index].name)
-      collateralAmmount.value = ((loan_amount/0.25)/parseFloat(elements[index].usd)).toFixed(6)
+      collateralAmount.value = ((loan_amount/0.25)/parseFloat(elements[index].usd)).toFixed(6)
+      // console.log((loan_amount/0.25)/parseFloat(elements[index].usd).toFixed(6))
+      break;
+    }
+  }
+}
+function setLoanValue(colleteral_amount) {
+  let init_currency = $('.currency-btns > .btn.active').text().trim()
+  let init_ltv = $('.collateral-ltv-btns > .btn.active').text().trim()
+  console.log(init_ltv.match(/\d+/g)/100)
+  document.getElementById("ca_logo").src= "assets/"+init_currency.toLowerCase()+".png"; 
+  let elements = listOfValuesOne;
+  for (let index = 0; index < elements.length; index++) {
+    if (elements[index].name === init_currency) {
+      loanAmount.value = ((colleteral_amount*parseFloat(elements[index].usd))*0.25).toFixed(0)
+      calculate_ca()
       // console.log((loan_amount/0.25)/parseFloat(elements[index].usd).toFixed(6))
       break;
     }
