83c9783688d166840db908c8817e336ad0b956be
Genievot <40867747+genievot@users.noreply.github.com>
2019-09-26 22:49:38 +0530

    JS HTML CSS Updated
    CSS
    - Added line management
    HTML
    - Template improved for small resolution screens
    
    JS
    - Completed basic calculations
    - Added formatting
    - Small features

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function latestData() {
         data.forEach(crypto => {
           listOfValuesOne.push({ name: crypto.name, usd: crypto.usd, interest: crypto.interestRate })
         })
+        console.log(listOfValuesOne)
         calculate_ca()
       } else {
         console.log('No data available!')
@@ -29,6 +30,12 @@ latestData();
 // select elements
 var loanAmount = document.getElementById('loan-amount')
 var collateralAmount = document.getElementById('collateral-amount')
+var aprCEL = document.getElementById('cel_apr')
+var pmCEL = document.getElementById('cel_pm')
+var totalCEL = document.getElementById('cel_total')
+var aprFiat = document.getElementById('fiat_apr')
+var pmFiat = document.getElementById('fiat_pm')
+var totalFiat = document.getElementById('fiat_total')
 // results cel
 
 // results fiat
@@ -57,6 +64,8 @@ $(".currency-btns label").click(function () {
 
 $(".collateral-ltv-btns label").click(function () {
   $(this).addClass('active').siblings().removeClass('active');
+  calculateCEL()
+  calculateFiat()
   if (changeBasedOnLA) {
     calculate_ca()
   } else {
@@ -64,18 +73,43 @@ $(".collateral-ltv-btns label").click(function () {
   }
 });
 
+$(".lt-btns label").click(function () {
+  $(this).addClass('active').siblings().removeClass('active');
+  calculateCEL()
+  calculateFiat()
+});
 
-function format(value) {
-  if (value >= 1000000000) return (value / 1000000000).toFixed(2) + 'B'
-  else if (value >= 1000000) return (value / 1000000).toFixed(2) + 'M'
+function format (labelValue) {
+
+  // Nine Zeroes for Billions
+  return Math.abs(Number(labelValue)) >= 1.0e+9
+
+  ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
+  // Six Zeroes for Millions 
+  : Math.abs(Number(labelValue)) >= 1.0e+6
+
+  ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
+  // Three Zeroes for Thousands
+  : Math.abs(Number(labelValue)) >= 1.0e+3
+
+  ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
+
+  : Math.abs(Number(labelValue));
+
 }
 
-function calculate_ca(e, i) {
+function getRawLAmount() {
   let value_raw_la = loanAmount.value.match(/\d+/g)
   let joined_value
-  console.log(value_raw_la)
-  if (value_raw_la) {
+  if (value_raw_la) { 
     joined_value = value_raw_la.join("")
+    return joined_value
+  }
+  return false
+}
+function calculate_ca(e, i) {
+  if (getRawLAmount()) {
+    let joined_value = getRawLAmount()
     // coloring border red to show something wrong with this input.
     if (joined_value < 1500) {
       $('#loan-amount').css('border-color', 'red');
@@ -83,25 +117,22 @@ function calculate_ca(e, i) {
     if (joined_value >= 1500) {
       $('#loan-amount').css('border-color', '');
     }
-    console.log(joined_value)
     loanAmount.value = formatter.format(joined_value)
     changeBasedOnLA = true
-    console.log(i)
     if (i) return; // So Settingup current LA from CA won't reset CA based on current LA
-    console.log(i)
-    console.log('skipping')
     setColleteralValue(joined_value)
+    calculateCEL()
+    calculateFiat()
   }
 }
 function validate_ca() {
-  let value_raw_la = loanAmount.value.match(/\d+/g)
   let joined_value
-  if (!value_raw_la) {
+  if (!getRawLAmount()) {
     loanAmount.value = '$1,500'
     setColleteralValue(1500)
     return
   }
-  joined_value = value_raw_la.join("")
+  joined_value = getRawLAmount()
   if (joined_value < 1500) {
     loanAmount.value = '$1,500'
     $('#loan-amount').css('border-color', '');
@@ -117,7 +148,6 @@ function validate_ca() {
 let old_right_val // Temp variable for calculate_la()
 function calculate_la() {
   let value_raw_ca = collateralAmount.value.match(/^[0-9]*\.?[0-9]*$/)
-  //.match(/(\d+(\.\d+)?)/g) good with lazy loading
   let joined_value
   if (value_raw_ca) {
     if (value_raw_ca[0] === '.') {
@@ -134,6 +164,8 @@ function calculate_la() {
   collateralAmount.value = joined_value
   changeBasedOnLA = false
   setLoanValue(joined_value)
+  calculateCEL()
+  calculateFiat()
 }
 
 function setColleteralValue(loan_amount) {
@@ -154,7 +186,6 @@ function setLoanValue(colleteral_amount) {
   let init_currency = $('.currency-btns > .btn.active').text().trim()
   let init_ltv = $('.collateral-ltv-btns > .btn.active').text().trim()
   let ltv_to_use = init_ltv.match(/\d+/g) / 100
-  console.log(init_ltv.match(/\d+/g) / 100)
   document.getElementById("ca_logo").src = "assets/" + init_currency.toLowerCase() + ".png";
   let elements = listOfValuesOne;
   for (let index = 0; index < elements.length; index++) {
@@ -166,4 +197,42 @@ function setLoanValue(colleteral_amount) {
   }
 }
 
+function calculateCEL() {
+  let init_ltv = $('.collateral-ltv-btns > .btn.active').text().trim()
+  let init_lt = $('.lt-btns > .btn.active').attr("value");
+  let apr_val = 3.46
+  // conditions for ltv
+  if (init_ltv === '25%') {
+    apr_val = 3.46
+  }
+  if (init_ltv === '33%') {
+    apr_val = 4.87
+  }
+  if (init_ltv === '50%') {
+    apr_val = 6.26
+  }
+  aprCEL.innerText = apr_val + '%'
+  let pm_CEL = ((getRawLAmount()*(apr_val/100))/12).toFixed(2)
+  pmCEL.innerText = '$' + format(pm_CEL)
+  totalCEL.innerText = '$' + format((init_lt * pm_CEL).toFixed(2))
+}
 
+function calculateFiat() {
+  let init_ltv = $('.collateral-ltv-btns > .btn.active').text().trim()
+  let init_lt = $('.lt-btns > .btn.active').attr("value");
+  let apr_val = 4.95
+  // conditions for ltv
+  if (init_ltv === '25%') {
+    apr_val = 4.95
+  }
+  if (init_ltv === '33%') {
+    apr_val = 6.95
+  }
+  if (init_ltv === '50%') {
+    apr_val = 8.95
+  }
+  aprFiat.innerText = apr_val + '%'
+  let pm_Fiat = ((getRawLAmount()*(apr_val/100))/12).toFixed(2)
+  pmFiat.innerText = '$' + format(pm_Fiat)
+  totalFiat.innerText = '$' + format((init_lt * pm_Fiat).toFixed(2))
+}
